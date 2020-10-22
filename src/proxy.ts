3a612d97a7feac175076d4adba6dbdeb9e016423
Leonid Beder <leonid.beder@gmail.com>
2020-10-22 23:59:58 +0300

    Add typed data return methods

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,9 +3,9 @@ import { defaultAbiCoder } from "@ethersproject/abi";
 import { toUtf8Bytes } from "@ethersproject/strings";
 import { pack } from "@ethersproject/solidity";
 
-export const ATTEST_TYPE =
+export const ATTEST_TYPED_SIGNATURE =
   "Attest(address recipient,uint256 ao,uint256 expirationTime,bytes32 refUUID,bytes data,uint256 nonce)";
-export const REVOKE_TYPE = "Revoke(byte32 uuid,uint256 nonce)";
+export const REVOKE_TYPED_SIGNATURE = "Revoke(byte32 uuid,uint256 nonce)";
 export const EIP712_DOMAIN = "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)";
 export const EIP712_NAME = "EAS";
 
@@ -50,6 +50,70 @@ export type Signature = { v: number; r: string; s: string };
 export type SignMessage = (message: Buffer) => Promise<Signature>;
 export type VerifyMessage = (message: Buffer, signature: Signature) => Promise<string>;
 
+export interface Attribute {
+  name: string;
+  type:
+    | "bool"
+    | "uint8"
+    | "uint16"
+    | "uint32"
+    | "uint64"
+    | "uint128"
+    | "uint256"
+    | "address"
+    | "string"
+    | "bytes"
+    | "bytes32";
+}
+
+export const ATTEST_PRIMARY_TYPE = "Attest";
+export const REVOKE_PRIMARY_TYPE = "Revoke";
+export const DOMAIN_TYPE: Attribute[] = [
+  { name: "name", type: "string" },
+  { name: "version", type: "string" },
+  { name: "chainId", type: "uint256" },
+  { name: "verifyingContract", type: "address" }
+];
+export const ATTEST_TYPE: Attribute[] = [
+  { name: "recipient", type: "address" },
+  { name: "ao", type: "uint256" },
+  { name: "expirationTime", type: "uint256" },
+  { name: "refUUID", type: "bytes32" },
+  { name: "data", type: "bytes" },
+  { name: "nonce", type: "uint256" }
+];
+export const REVOKE_TYPE: Attribute[] = [
+  { name: "uuid", type: "bytes32" },
+  { name: "nonce", type: "uint256" }
+];
+
+export interface EIP712DomainTypedData {
+  chainId: number;
+  name: string;
+  verifyingContract: string;
+  version: string;
+}
+
+export interface EIP712AttestationTypedData {
+  domain: EIP712DomainTypedData;
+  primaryType: typeof ATTEST_PRIMARY_TYPE;
+  types: {
+    EIP712Domain: typeof DOMAIN_TYPE;
+    Attest: typeof ATTEST_TYPE;
+  };
+  message: EIP712AttestationParams;
+}
+
+export interface EIP712RevocationTypedData {
+  domain: EIP712DomainTypedData;
+  primaryType: typeof REVOKE_PRIMARY_TYPE;
+  types: {
+    EIP712Domain: typeof DOMAIN_TYPE;
+    Revoke: typeof REVOKE_TYPE;
+  };
+  message: EIP712RevocationParams;
+}
+
 export class Proxy {
   private eip712Config: EIP712Config;
 
@@ -70,6 +134,15 @@ export class Proxy {
         ]
       )
     );
+  }
+
+  public getDomainTypedData(): EIP712DomainTypedData {
+    return {
+      chainId: this.eip712Config.chainId,
+      name: EIP712_NAME,
+      verifyingContract: this.eip712Config.address,
+      version: this.eip712Config.version
+    };
   }
 
   public async getAttestationRequest(
@@ -101,6 +174,18 @@ export class Proxy {
     return attester === recoveredAddress;
   }
 
+  public getAttestationTypedData(params: EIP712AttestationParams): EIP712AttestationTypedData {
+    return {
+      domain: this.getDomainTypedData(),
+      primaryType: ATTEST_PRIMARY_TYPE,
+      message: params,
+      types: {
+        EIP712Domain: DOMAIN_TYPE,
+        Attest: ATTEST_TYPE
+      }
+    };
+  }
+
   public async getRevocationRequest(
     params: EIP712RevocationParams,
     signMessage: SignMessage
@@ -130,6 +215,18 @@ export class Proxy {
     return attester === recoveredAddress;
   }
 
+  public getRevocationTypedData(params: EIP712RevocationParams): EIP712RevocationTypedData {
+    return {
+      domain: this.getDomainTypedData(),
+      primaryType: REVOKE_PRIMARY_TYPE,
+      message: params,
+      types: {
+        EIP712Domain: DOMAIN_TYPE,
+        Revoke: REVOKE_TYPE
+      }
+    };
+  }
+
   private getAttestationDigest(params: EIP712AttestationParams): string {
     return keccak256(
       pack(
@@ -142,7 +239,7 @@ export class Proxy {
             defaultAbiCoder.encode(
               ["bytes32", "address", "uint256", "uint256", "bytes32", "bytes", "uint256"],
               [
-                keccak256(toUtf8Bytes(ATTEST_TYPE)),
+                keccak256(toUtf8Bytes(ATTEST_TYPED_SIGNATURE)),
                 params.recipient,
                 params.ao,
                 params.expirationTime,
@@ -168,7 +265,7 @@ export class Proxy {
           keccak256(
             defaultAbiCoder.encode(
               ["bytes32", "bytes32", "uint256"],
-              [keccak256(toUtf8Bytes(REVOKE_TYPE)), params.uuid, params.nonce]
+              [keccak256(toUtf8Bytes(REVOKE_TYPED_SIGNATURE)), params.uuid, params.nonce]
             )
           )
         ]
