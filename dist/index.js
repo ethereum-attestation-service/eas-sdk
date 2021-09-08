96953b3800a3d7319c544ac957f417d6861ac1e8
Leonid Beder <leonid.beder@gmail.com>
2021-09-08 11:11:01 +0100

    Update dependencies

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var tslib_1 = require("tslib");
-tslib_1.__exportStar(require("./delegation"), exports);
+(0, tslib_1.__exportStar)(require("./delegation"), exports);
 //# sourceMappingURL=index.js.map
