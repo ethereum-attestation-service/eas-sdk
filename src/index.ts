426e6081ada8f160c3a1907047981ba415d20ace
lbeder <lbeder@users.noreply.github.com>
2022-11-20 21:19:49 +0000

    Export offchain and delegated attestations

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-export * from './schema-registry';
 export * from './eas';
+export * from './offchain';
+export * from './schema-registry';
---
+++ src/offchain/index.ts
@@ -0,0 +1,2 @@
+export * from './delegation';
+export * from './offchain';
