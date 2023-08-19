c7a4992532f95c786034ad4366a3ed60fb07989a
Ian Lai <ian@fyianlai.com>
2023-08-19 13:11:36 +0100

    Fix lint

--- test/test/schema-encoder.ts
+++ test/test/schema-encoder.ts
@@ -178,7 +178,7 @@ describe('SchemaEncoder', () => {
           {
             in: [
               { type: 'address[]', name: 'contractAddresses', value: ['0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', '0x70997970C51812dc3A010C7d01b50e0d17dc79C8'] },
-              { type: 'string[]', name: 'names', value: ["abc", "def"] }
+              { type: 'string[]', name: 'names', value: ['abc', 'def'] }
             ]
           }
         ]
