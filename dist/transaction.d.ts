359d229374549bdbd7b06c94969f23dd46b75cf6
lbeder <leonid@lbeder.com>
2024-04-13 15:34:01 +0100

    Don't require the signer/provider to have the `getAddress` method

--- dist/transaction.d.ts
+++ dist/transaction.d.ts
@@ -1,5 +1,5 @@
-import { Addressable, BaseContract, ContractFactory, ContractTransaction, TransactionReceipt, TransactionRequest } from 'ethers';
-export interface TransactionSigner extends Addressable {
+import { BaseContract, ContractFactory, ContractTransaction, TransactionReceipt, TransactionRequest } from 'ethers';
+export interface TransactionSigner {
     estimateGas: (tx: TransactionRequest) => Promise<bigint>;
     sendTransaction: (tx: TransactionRequest) => Promise<any>;
     call: (tx: TransactionRequest) => Promise<string>;
